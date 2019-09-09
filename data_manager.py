3e5d8cab0906571b37e287fc7f7e35dee6cf7c9f
JenesesD <jenesesd@gmail.com>
2019-09-09 16:13:27 +0200

    implemented assisting functions

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,33 @@
 import connection as cn
-
+from datetime import datetime
 
 @cn.connection_handler
-def display_questions(cursor):
-    query = """SELECT * FROM question"""
+def display_questions(cursor, table):
+    query = f"""SELECT * FROM {table}"""
     cursor.execute(query)
     data = cursor.fetchall()
     return data
+
+def get_next_id(cursor, table):
+    query = f"""SELECT id FROM {table} ORDER BY id DESC LIMIT 1"""
+    cursor.execute(query)
+    last_id = cursor.fetchone()
+    next_id = last_id["id"] + 1
+    return next_id
+
+
+def replace_commas(data):
+    data = dict(data)
+    for column in data:
+        data[column] = data[column].replace("'", "''")
+    return data
+
+
+def check_current_time(data):
+    data["submission_time"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return data
+
+
+@cn.connection_handler
+def add_new_data_to_table(cursor, table):
+    pass
--- server.py
+++ server.py
@@ -12,45 +12,32 @@ answer_path = "sample_data/answer.csv"
 @app.route('/')
 @app.route('/list', methods=['POST', 'GET'])
 def route_list():
-    data = dm.display_questions()
+    data = dm.display_questions('question')
     return render_template('list.html', sorted_questions=data)
 
 
-@app.route('/question/<question_id>', methods=['POST', 'GET'])
-def expand_question(question_id):
-    if request.method == 'GET':
-        all_questions = cn.get_all_data_from_file(question_path)
-        all_answers = cn.get_all_data_from_file(answer_path)
-        return render_template('question.html', question_id=question_id, answers=all_answers, questions=all_questions)
-    else:
-        if request.method == "POST":
-            id_ = int(dm.generate_new_id(answer_path))
-            submission_time = int(time.time())
-            vote_number = 0
-            question_id = request.form.get("question_id")
-            message = request.form.get("answer")
-            image = request.form.get("image")
-            new_answer = [id_, submission_time, vote_number, question_id, message, image]
-            cn.add_new_data_to_csv(answer_path, new_answer)
-            return redirect('/question/'+str(question_id))
+# @app.route('/question/<question_id>', methods=['POST', 'GET'])
+# def expand_question(question_id):
+    # if request.method == 'GET':
+    #     all_questions = cn.get_all_data_from_file(question_path)
+    #     all_answers = cn.get_all_data_from_file(answer_path)
+    #     return render_template('question.html', question_id=question_id, answers=all_answers, questions=all_questions)
+    # else:
+    #     if request.method == "POST":
+    #         id_ = int(dm.generate_new_id(answer_path))
+    #         submission_time = int(time.time())
+    #         vote_number = 0
+    #         question_id = request.form.get("question_id")
+    #         message = request.form.get("answer")
+    #         image = request.form.get("image")
+    #         new_answer = [id_, submission_time, vote_number, question_id, message, image]
+    #         cn.add_new_data_to_csv(answer_path, new_answer)
+    #         return redirect('/question/'+str(question_id))
 
 
 @app.route('/add_question', methods=['POST', 'GET'])
-def add_question():
-    """Add new question to list, then redirect to /list page"""
-    if request.method == 'POST':
-        id_ = dm.generate_new_id(question_path)
-        submission_time = int(time.time())
-        view_number = 0
-        vote_number = 0
-        title = request.form["title"]
-        message = request.form["message"]
-        image = 0
-        new_question = [id_, submission_time, view_number, vote_number, title, message, image]
-        cn.add_new_data_to_csv(question_path, new_question)
-        return redirect('/list')
-    else:
-        return render_template('add_question.html')
+def route_add_question():
+    pass
 
 
 if __name__ == '__main__':
