40e914a9593d67d5f2cb3cac4102f9b7e3ea0c65
Bozi Daniel <daniel.bozi88@gmail.com>
2019-09-12 16:48:38 +0200

    add delete comment meg már nem emlékszem mi de nem atomic az biztos

--- data_manager.py
+++ data_manager.py
@@ -8,19 +8,17 @@ def display_table(cursor, table):
     cursor.execute(query)
     data = cursor.fetchall()
     return data
-'''
-    if sort_by == "submission_time" and order == "descending":
-        cursor.exexcute(""" SELECT * FROM question 
-        ORDER BY %s DESC
-        """)
-'''
 
+    # if sort_by == "submission_time" and order == "descending":
+    #     cursor.exexcute(""" SELECT * FROM question
+    #     ORDER BY %s DESC
+    #     """)
 
 
 @cn.connection_handler
-def get_question_by_id(cursor, id):
-    query = f"""SELECT * FROM question WHERE id = {id}"""
-    cursor.execute(query)
+def get_question_by_id(cursor, id_):
+    cursor.execute("""SELECT * FROM question
+                        WHERE id= %(id)s""", {'id': id_})
     data = cursor.fetchall()
     return data
 
@@ -31,14 +29,6 @@ def get_answer_by_id(cursor, question_id):
     cursor.execute(query)
     data = cursor.fetchall()
     return data
-
-
-def get_last_id(cursor, table):
-    query = f"""SELECT id FROM {table} ORDER BY id DESC LIMIT 1"""
-    cursor.execute(query)
-    last_id = cursor.fetchone()
-    next_id = last_id["id"] + 1
-    return next_id
 
 
 def replace_commas(data):
@@ -89,16 +79,6 @@ def update_row_in_table(cursor, table, data):
 
 
 @cn.connection_handler
-def delete_question(cursor, question_id):
-    query1 = f""" DELETE FROM question
-                WHERE id= {question_id};"""
-    query2 = f""" DELETE FROM answer
-                WHERE question_id= {question_id};"""
-    cursor.execute(query2)
-    cursor.execute(query1)
-
-
-@cn.connection_handler
 def display_latest_questions(cursor):
     cursor.execute("""
                     SELECT id ,submission_time , title FROM question ORDER BY submission_time DESC LIMIT 5""")
@@ -108,7 +88,7 @@ def display_latest_questions(cursor):
 
 @cn.connection_handler
 def get_all_comments_to_a_question(cursor, question_id):
-    query = f"""SELECT submission_time, message FROM comment
+    query = f"""SELECT submission_time, message, id FROM comment
             WHERE question_id={question_id}"""
     cursor.execute(query)
     comments_to_a_question = cursor.fetchall()
@@ -117,9 +97,66 @@ def get_all_comments_to_a_question(cursor, question_id):
 
 @cn.connection_handler
 def add_comment_to_question(cursor, data):
-    query = f"""INSERT INTO comment (question_id, message, submission_time)
-                VALUES ('{data["question_id"]}', '{data["comment"]}', 
+    comment_id = generate_comment_id()
+    data["id"] = comment_id
+    query = f"""INSERT INTO comment (question_id, id, message, submission_time)
+                VALUES ('{data["question_id"]}', '{data["id"]}', '{data["comment"]}', 
                         '{data["submission_time"]}')"""
+
     cursor.execute(query)
 
 
+@cn.connection_handler
+def generate_comment_id(cursor):
+    cursor.execute("""SELECT id FROM comment 
+                    ORDER BY id DESC LIMIT 1;""")
+    last_id = cursor.fetchone()
+    next_id = last_id['id'] + 1
+    print(next_id)
+    return next_id
+#Jenci erre írt egy jobbat táblás paraméterrel de az most nincs meg meg nem működött, írtam egy baltábbat de lehet erre sincs szükség
+# cs gondoltam szebb ha nem az adatbázis generálja az id-t.
+
+
+@cn.connection_handler
+def delete_comment(cursor, id_):
+    cursor.execute("""DELETE FROM comment 
+                    WHERE id= %(id)s""", {'id': id_})
+
+
+@cn.connection_handler
+def delete_answer(cursor, id_):
+    cursor.execute("""DELETE FROM comment
+                    WHERE answer_id= %(id_)s""", {'id_': id_});
+
+    cursor.execute("""DELETE FROM answer
+                    WHERE id= %(id_)s""", {'id_': id_});
+
+
+@cn.connection_handler
+def delete_question(cursor, question_id):
+    query1 = f""" DELETE FROM question
+                  WHERE id= {question_id};"""
+    query2 = f""" DELETE FROM answer
+                  WHERE question_id= {question_id};"""
+    query3 = f"""DELETE FROM comment
+                  WHERE question_id={question_id};"""
+    cursor.execute(query3)
+    cursor.execute(query2)
+    cursor.execute(query1)
+
+
+@cn.connection_handler
+def get_question_id_by_comment_id(cursor, comment_id):
+    cursor.execute("""SELECT question_id FROM comment
+                    WHERE id= %(comment_id)s""", {'comment_id': comment_id})
+    question_id = cursor.fetchall()
+    return question_id
+
+
+@cn.connection_handler
+def get_question_id_by_answer_id(cursor, answer_id):
+    cursor.execute("""SELECT question_id FROM answer
+                    WHERE id=%(answer_id)s""", {'answer_id': answer_id})
+    question_id = cursor.fetchall()
+    return question_id
--- server.py
+++ server.py
@@ -14,9 +14,8 @@ def route_list():
 
 @app.route('/question/<question_id>', methods=['GET'])
 def route_expand_question(question_id):
+    print(question_id)
     question = dm.get_question_by_id(question_id)
-    # question["view_number"] = int(question["view_number"]) + 1
-    # dm.update_row_in_table("question", question)
     answers_by_question_id = dm.get_answer_by_id(question_id)
     comments_to_question = dm.get_all_comments_to_a_question(question_id)
     return render_template('question.html', question=question, answers=answers_by_question_id, question_id=question_id, comments=comments_to_question)
@@ -51,17 +50,42 @@ def route_delete_question(question_id):
 
 @app.route('/question/<question_id>/new-comment', methods=['POST'])
 def route_comment_question(question_id):
+    print(question_id)
     data = request.form
     data = dict(data)
     data = dm.check_current_time(data)
     dm.add_comment_to_question(data)
-    return redirect(url_for('route_expand_question', question_id=question_id))
+    comment_id = data['id']
+    return redirect(url_for('route_expand_question', question_id=question_id, comment_id=comment_id))
 
 
 @app.route('/latest_questions', methods=['GET', 'POST'])
 def route_latest_questions():
     data = dm.display_latest_questions()
     return render_template('latest_questions.html', latest_questions=data)
+
+
+# @app.route('/delete_comment/are_you_sure/<comment_id>/<question_id>')
+# def route_are_you_sure(comment_id, question_id):
+#     return render_template('are_you_sure.html', comment_id=comment_id, question_id=question_id)
+
+
+@app.route('/comments/<comment_id>/delete')
+def route_delete_comment(comment_id):
+    question_id = dm.get_question_id_by_comment_id(comment_id)
+    dm.delete_comment(comment_id)
+    question_id = dict(question_id[0])
+    question_id = question_id['question_id']
+    return redirect(url_for('route_expand_question', question_id=question_id, ))
+
+
+@app.route('/answer/<answer_id>/delete')
+def route_delete_answer(answer_id):
+    question_id = dm.get_question_id_by_answer_id(answer_id)
+    question_id = dict(question_id[0])
+    question_id = question_id['question_id']
+    dm.delete_answer(answer_id)
+    return redirect(url_for('route_expand_question', question_id=question_id))
 
 
 if __name__ == '__main__':
